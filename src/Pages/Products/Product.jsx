7ece4370d59991e48a2f3dde93e28a02722f0d44
Valentin <valentinarpin11@live.fr>
2023-02-02 17:23:27 +0100

    [add] cards

--- src/Pages/Products/Product.jsx
+++ src/Pages/Products/Product.jsx
@@ -1,7 +1,31 @@
+import heart from './heart.svg'
+import './Product.css'
+import inventory from "../../data/inventory";
+import {Link} from "react-router-dom";
+
 export default function Product() {
     return(
-        <div>
-
+        <div className="container-products">
+            {inventory.map((item) => (
+                <Link to={{
+                    pathname: `/produits/${item.title.replace(/\s+/g, "").
+                    trim()}`,
+                }}
+                key={item.id}
+                >
+                <div className="bloc-card">
+                    <div className="product-card">
+                        <div className="visual-aspect">
+                            <img
+                            className="img-product"
+                            src={process.env.PUBLIC_URL + `/images/${item.img}.png`
+                            }
+                            alt="produit"/>
+                        </div>
+                    </div>
+                </div>
+                </Link>
+            ))}
         </div>
     )
 }
