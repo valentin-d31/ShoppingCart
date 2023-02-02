bd14bdf77494cab4da8e55480bbff2aa949cb938
Valentin <valentinarpin11@live.fr>
2023-02-02 16:27:27 +0100

    [add] navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 export default function Navbar() {
     return(
         <div>
+            <nav className="nav-top">
+                <Link to="/">ACUUEIL</Link>
+                <Link to="/produits">PRODUITS</Link>
+                <Link to="/contact">CONTACT</Link>
 
+            </nav>
         </div>
     )
 }
