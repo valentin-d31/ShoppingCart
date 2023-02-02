de0be0873f4d3dcd75bf000e43201f29b743507c
Valentin <valentinarpin11@live.fr>
2023-02-02 16:09:40 +0100

    [Add] Cart

--- src/Components/FloatingCart/FloatingCart.jsx
+++ src/Components/FloatingCart/FloatingCart.jsx
@@ -1,7 +1,19 @@
-export default function FloatingCart() {
-    return(
-        <div>
+import cartIcon from './shopping-cart.svg';
+import './FloatingCart.css'
+import {Link} from "react-router-dom";
 
-        </div>
+export default function FloatingCart() {
+    //1.element cart increment à droite
+    return(
+        <Link to={}>
+            <div className="floating-cart">
+                <p>Votre Panier</p>
+                <div className="img-notif-container">
+                    <img src={cartIcon} alt="icone cadi"/>
+                    <span className="notif">0</span>
+                </div>
+            </div>
+        </Link>
+
     )
 }
