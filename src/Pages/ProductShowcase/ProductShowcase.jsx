7f8f80e3aa674b933a734f8af90fd33680cef676
Valentin <valentinarpin11@live.fr>
2023-02-04 15:05:55 +0100

    [add] img product

--- src/Pages/ProductShowcase/ProductShowcase.jsx
+++ src/Pages/ProductShowcase/ProductShowcase.jsx
@@ -6,14 +6,22 @@ export default function ProductShowcase() {
 
     const {id} = useParams()
     //findIndex=>trouve l'index d'un objet dans un tbl d'object
-    //.Comparaison id du table et l'id sur lequel je clic
+    //.Comparaison id du table d'obj et l'id sur lequel je clic
     const productClicked = inventory.findIndex(obj => obj.title
         .replace(/\s+/g, "").
         trim() === id)
 
     return(
-        <div>
-
+        <div className="showcase">
+            <div className="container-img-showcase">
+                <img
+                    className="img-showcase"
+                    //chaine de caractère=[]l'img de l'id sur lequel je clic
+                    src={process.env.PUBLIC_URL +
+                        `/images/${inventory[productClicked].img}.png`}
+                    alt=""
+                />
+            </div>
         </div>
     )
 }
